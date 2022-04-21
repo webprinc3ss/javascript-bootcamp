a83738b3a335242d00a835f45211aa707f475131
Danielle Branton <djabranton@gmail.com>
2022-04-20 20:19:13 -0700

    Thermometer challenge.

---
+++ basics/tempconversion.js
@@ -0,0 +1,8 @@
+let fahrenheit = 32 // 0 degrees in C and 273.15K
+let celcius = (fahrenheit - 32) / 1.8
+let kelvin = celcius + 273.15
+console.log ('celcius: ' + celcius)
+console.log ('kelvin: ' + kelvin)
+
+//calculate kelvin and store in variable rapidtables.com
+//print that value
