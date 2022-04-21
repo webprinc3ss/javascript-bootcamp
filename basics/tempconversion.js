11104b1cfd7fbd756f3ddc4f9ebd746d653ce376
Danielle Branton <djabranton@gmail.com>
2022-04-21 08:15:50 -0700

    Commit

--- basics/tempconversion.js
+++ basics/tempconversion.js
@@ -1,4 +1,4 @@
-let fahrenheit = 32 // 0 degrees in C and 273.15K
+let fahrenheit = 50 // 0 degrees in C and 273.15K
 let celcius = (fahrenheit - 32) / 1.8
 let kelvin = celcius + 273.15
 console.log ('celcius: ' + celcius)
