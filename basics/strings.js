487dfaf2cc8e4d714d358eacbee65757d65672d5
Danielle Branton <djabranton@gmail.com>
2022-04-20 19:36:55 -0700

    Strings challenge.

--- basics/strings.js
+++ basics/strings.js
@@ -1,6 +1,5 @@
-let firstName = 'Danielle';
-let lastName = 'Branton';
-let fullName = firstName +  ' ' + lastName;
-
-console.log(fullName);
+let city = 'Las Vegas';
+let country = 'United Sta+tes';
+let location = city + ', ' + country;
+console.log (location)
 
