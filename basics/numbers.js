219422204be82f7c9bf29f6e62d2551a2069398d
Danielle Branton <djabranton@gmail.com>
2022-04-20 19:57:55 -0700

    Numbers

---
+++ basics/numbers.js
@@ -0,0 +1,17 @@
+//let x = 11
+//let num = (x + 1) * 2
+//console.log(num)
+
+//let age = 53
+//let dogYears = (age + 1) / 7
+
+//console.log(dogYears)
+
+//Challenge Area
+
+let studentScore = 18
+let maxScore = 20
+//percent 90
+let percent = (studentScore/maxScore) * 100
+// 18/20 * 100 = percent
+console.log (percent + '%')  
