355a2f24dfafca29e517719ecb5175e591f95b07
Danielle Branton <djabranton@gmail.com>
2022-04-22 09:56:49 -0700

    Boolean challenge.

---
+++ basics/boolean.js
@@ -0,0 +1,24 @@
+// === is equality operator
+// !== is NOT equal operator
+// >  operator
+// < operator
+// >=
+// >=
+let temp = 32
+let isFreezing = temp >= 100
+
+console.log(isFreezing)
+
+
+//Challenge
+
+//Create age variable set to your age
+let age = 53
+//Calculate is child - if they are 7 or under
+let isChild = age >= 7
+//Calculate is senior - if they are 65 or older
+let isSenior = age >= 65
+//Console.log is child value
+console.log ('isChild: ' + isChild)
+//Console.log is senior value
+console.log ('isSenior: ' + isSenior)
--- basics/variables.js
+++ basics/variables.js
@@ -1,4 +1,12 @@
+//Can't define a variable more than once
 let petName = 'Hal'
+
 petName = 'Spot'
 
 console.log (petName)
+
+//Rules related to variable names
+    //First character must start with letter, _ , $
+
+//Variable names must not be reserved keywords
+    //ex. let
