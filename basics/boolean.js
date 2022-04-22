13d159013d27e6e031afecf7a0ced72af5bddea9
Danielle Branton <djabranton@gmail.com>
2022-04-22 10:19:48 -0700

    Conditional challenge.

--- basics/boolean.js
+++ basics/boolean.js
@@ -4,8 +4,17 @@
 // < operator
 // >=
 // >=
-let temp = 32
-let isFreezing = temp >= 100
+let temp = 180
+let isFreezing = temp <= 32
+
+if (isFreezing) {
+    console.log ('It is freezing outside!')
+}
+
+if (temp >= 110) {
+    console.log ('It is way to hot outside!')
+    console.log('Testing123')
+}
 
 console.log(isFreezing)
 
@@ -15,10 +24,22 @@ console.log(isFreezing)
 //Create age variable set to your age
 let age = 53
 //Calculate is child - if they are 7 or under
-let isChild = age >= 7
+let isChild = age <= 7
 //Calculate is senior - if they are 65 or older
 let isSenior = age >= 65
 //Console.log is child value
 console.log ('isChild: ' + isChild)
 //Console.log is senior value
 console.log ('isSenior: ' + isSenior)
+
+
+//Challenge 2
+// If 7 or under print message bout child pricing
+if (isChild){
+    console.log('You get the child discount!')
+}
+//If 65 or older print message about senior pricing
+if (isSenior) {
+    console.log ('You get the senior discount!')
+}
+
