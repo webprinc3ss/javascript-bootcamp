3a74998494b13b772f69f9209fbb0738c0bf06dc
Danielle Branton <djabranton@gmail.com>
2022-05-02 08:28:28 -0700

    Function101

--- functions/functions101.js
+++ functions/functions101.js
@@ -18,15 +18,15 @@ console.log(value, otherValue)
 // Challenge Area
 
     // convertFahrenheittoCelcius
-let convert = function (num) {
-    let result = (num - 32) / 1.8
-    return result
+let convertFahrenheittoCelcius = function (farenheit) {
+    let celcius = (farenheit - 32) / 1.8
+    return celcius
 }
 // Call a couple of times (32-0) (68-20)
-let c = convert(32)
-let c2 = convert(68)
+let temp1 = convertFahrenheittoCelcius(32)
+let temp2 = convertFahrenheittoCelcius(68)
 
 // Print the converted values
-console.log(c, c2)
+console.log(temp1, temp2)
 
 
