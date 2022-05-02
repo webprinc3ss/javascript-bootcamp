25aacabd640e4d5deb5c9a82e030c0f684a30502
Danielle Branton <djabranton@gmail.com>
2022-05-02 11:00:36 -0700

    Undefined and null

---
+++ functions/undefined-null.js
@@ -0,0 +1,27 @@
+//undefined variable
+let name
+name='Jenn'
+if (name === undefined) {
+    console.log('Please provide a name')
+
+} else {
+    console.log(name)
+}
+
+//undefinted for function arguments
+let square = function (num) {
+    console.log (num)
+}
+
+//square ()
+
+//undefined as the function return default value
+let result = square ()
+console.log (result)
+
+//Null as assigned value
+let age = 27
+
+age = null
+
+console.log (age)
