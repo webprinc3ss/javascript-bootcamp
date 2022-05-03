d0e4355641ea835f1220cb56d5b8bac9844f74d0
Danielle Branton <djabranton@gmail.com>
2022-05-03 08:58:07 -0700

    Arguments challenge.

---
+++ functions/arguments.js
@@ -0,0 +1,25 @@
+//Multiple arguments
+
+let add = function (a, b, c) {
+    return a + b + c
+}
+let result = add (10, 1, 5)
+console.log(result)
+
+//Default arguments
+let getScoreText = function (name = 'Anonymous', score = 0) {
+    return 'Name: ' + name + ' ' + '- Score: ' + score
+}
+let scoreText = getScoreText(undefined, 99)
+console.log(scoreText)
+
+//Challenge Area
+//total, tipPercent .2
+
+let getTip = function (total, tipPercent = .18){
+return total * tipPercent
+}
+
+let tip = getTip(50)
+tipDecimal = tip.toFixed(2)
+console.log('Tip: $' + tipDecimal)
