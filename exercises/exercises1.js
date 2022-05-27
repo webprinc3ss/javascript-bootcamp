86a583d064776a75136605481ea54a5ff33db008
Danielle Branton <djabranton@gmail.com>
2022-05-27 09:44:01 -0700

    Methods on Objects challenge.

---
+++ exercises/exercises1.js
@@ -0,0 +1,5 @@
+//Write a JavaScript program to display the current day and time in the following format.
+//Sample Output : Today is : Tuesday.
+//Current time is : 10 PM : 30 : 38
+
+let dateTime = now()
--- objects/objects-methods.js
+++ objects/objects-methods.js
@@ -3,7 +3,23 @@ let restaurant = {
     guestCapacity: 75,
     guestCount:  0,
     checkAvailability: function (partySize) {
-console.log (partySize)
+        //console.log(this.guestCapacity)
+        let seatsLeft = this.guestCapacity - this.guestCount
+        //return this
+        return partySize <= seatsLeft
+    },
+    seatParty: function  (partySize) {
+        this.guestCount = this.guestCount + partySize
+    },
+    removeParty: function (partySize) {
+        this.guestCount = this.guestCount - partySize
     }
+
 }
 
+//seatParty
+//removeParty
+restaurant.seatParty (72)
+console.log (restaurant.checkAvailability (4))
+restaurant.removeParty(5)
+console.log (restaurant.checkAvailability (4))
