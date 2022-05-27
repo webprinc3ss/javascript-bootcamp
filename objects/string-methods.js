eeda4b069f73d697fd00dd7aa2689f24a3240457
Danielle Branton <djabranton@gmail.com>
2022-05-27 10:49:57 -0700

    Ternery and string method challenge.

---
+++ objects/string-methods.js
@@ -0,0 +1,34 @@
+let myName = ' Danielle Branton '
+
+//Length property
+
+console.log(myName.length)
+
+//Convert to Upper Case
+//myName.toUpperCase()
+console.log(myName.toUpperCase())
+
+//Convert to Lower Case
+console.log(myName.toLocaleLowerCase())
+
+//Includes
+let password = 'abc123asdljafklj098'
+console.log(password.includes('password'))
+
+//Trim
+console.log(myName.trim())
+
+//Challengage
+//isValidPassword
+//length is more than 8 - and it doesn't contain word 'password'
+
+let isValidPassword = function (password) {
+    return password.length < 9 || password.includes('password') ? false
+           : true
+
+
+}
+
+console.log(isValidPassword('asdfp'))
+console.log(isValidPassword('abc123@#$adsfadsf'))
+console.log(isValidPassword('asdfafafasdfpassword'))
