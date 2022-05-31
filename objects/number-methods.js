a2d189a5a323f3e57f65e741e8788dbfa59bf00e
Danielle Branton <djabranton@gmail.com>
2022-05-31 10:10:24 -0700

    Random Number guessing game

---
+++ objects/number-methods.js
@@ -0,0 +1,38 @@
+let num = 103.941
+
+//to fixed specifies decimal places
+
+console.log(num.toFixed(2))
+
+//math round
+console.log(Math.round(num))
+
+//Round down to nearest integer
+console.log(Math.floor(num))
+
+//Ceiling - round up to nearest integer
+console.log(Math.ceil(num))
+
+//Math.random between 0 and .99999
+let randomNum = Math.random()
+console.log(randomNum)
+
+//New range from 10-20 - Use common formula below
+let min = 10
+let max = 20
+randomNum = Math.floor(Math.random() * (max-min + 1)) + min
+console.log(randomNum)
+
+//Challenge
+    //Guessing Game
+    //1-5 - true if correct, false if not correct
+
+let makeGuess = function (guess) {
+    let min = 1
+    let max = 5
+    randomNum = Math.floor(Math.random() * (max-min + 1)) + min
+    console.log (randomNum)
+    return guess == randomNum 
+}
+
+console.log(makeGuess(2))
