95b969cc7f1b38767deff32add11e8aa34f19c52
Danielle Branton <djabranton@gmail.com>
2022-06-01 17:35:09 -0700

    Array challenge - todo list

---
+++ arrays/todo.js
@@ -0,0 +1,10 @@
+//Create an Array of todos
+    //5 todos
+    //You have x todos. x = length of array
+    //Print 1st and second to last items  > Todo: walk the dog
+
+const todos = ['Let the dogs out.', 'Work out.', 'Pick up Rey.', 'Food shop.', 'Cook dinner.']
+
+console.log(`You have ${todos.length} todos!`)
+console.log(`Todo 1: ${todos[0]}`)
+console.log(`Todo 5: ${todos[4]}`)
