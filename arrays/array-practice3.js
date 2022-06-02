d36bd175226ba8ae18e1a18e2875d2158b33e446
Danielle Branton <djabranton@gmail.com>
2022-06-02 15:06:27 -0700

    Print out elements of an array in reverse order.

---
+++ arrays/array-practice3.js
@@ -0,0 +1,18 @@
+// Using a for loop output the elements in reverse order
+
+let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+
+for (let i = arr.length-1; i >= 0; i --) {
+
+    console.log(arr[i])
+
+}
+
+
+
+
+
+
+
+// Example output: 
+// true 3.5  be  false cannot  true 9 what 43 OR each item on a new line
