ad490ae06a63a357527b5a89a9a2c12dc70b7f77
Danielle Branton <djabranton@gmail.com>
2022-06-02 08:01:15 -0700

    Array methods

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,11 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
-console.log (notes.length)
-console.log(notes[notes.length-1]) //Grab last item from list
+console.log(notes.pop())
+notes.push('My new note')
+
+console.log(notes.shift())
+notes.unshift('My first note')
+
+console.log(notes.length)
+console.log(notes)
+//console.log(notes[notes.length-1]) //Grab last item from list
