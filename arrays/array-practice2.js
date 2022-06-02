51da6e2f157b0706fcef243282a1f7c1f2acb65f
Danielle Branton <djabranton@gmail.com>
2022-06-02 14:46:09 -0700

    Print all even numbers within a range.

---
+++ arrays/array-practice2.js
@@ -0,0 +1,12 @@
+//Using a for loop print all even numbers up to and including n. Don’t include 0.
+
+let n1 = 22
+
+for (count = 2; count <= n1; count++) {
+    if (count % 2 === 0) {
+        console.log(count)
+    }
+}
+
+//Example output:
+// 2 4 6 8 10 12 14 16 18 20 22 Or each item on a new line.
