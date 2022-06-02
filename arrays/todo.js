a14374211c5008f84e7518c8543d97b6e8a55848
Danielle Branton <djabranton@gmail.com>
2022-06-02 08:57:59 -0700

    Added shift and splice methods.

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,10 +1,15 @@
-//Create an Array of todos
-    //5 todos
-    //You have x todos. x = length of array
-    //Print 1st and second to last items  > Todo: walk the dog
+
 
 const todos = ['Let the dogs out.', 'Work out.', 'Pick up Rey.', 'Food shop.', 'Cook dinner.']
 
+//Delete the third item
+//Add a new item onto the end
+todos.splice(4,4,'Bake a Cake')
+
+//Remove the first item from the list
+todos.shift()
+
 console.log(`You have ${todos.length} todos!`)
-console.log(`Todo 1: ${todos[0]}`)
-console.log(`Todo 5: ${todos[4]}`)
+console.log(todos)
+// console.log(`Todo 1: ${todos[0]}`)
+// console.log(`Todo 5: ${todos[4]}`)
