d179d39cdb5e718d1122b852157aabbb22a74574
Danielle Branton <djabranton@gmail.com>
2022-06-09 09:12:55 -0700

    Updated todo function to evaluate index variable.

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,4 +1,3 @@
-// const todos = ['Let the dogs out.', 'Work out.', 'Buy food', 'Food shop.', 'Cook dinner.']
 //1. Convert array to Array of objects -> text, completed property (boolean)
 
 const todos = [{
@@ -11,7 +10,7 @@ const todos = [{
     },
     {
         text: 'Buy food.',
-        completed: false
+        completed: true
     },
     {
         text: 'Food shop.',
@@ -23,27 +22,18 @@ const todos = [{
     }
 ]
 
-
 //2. Create a function that works with array of objects to remove a todo by text value
 
-// const findNote = function (notes, noteTitle) {
-//     const index = notes.findIndex(function (note, index) {
-//         return note.title.toLowerCase() === noteTitle.toLowerCase()
-//     })
-//     return notes[index]
-// }
-
-
-
 const deleteTodo = function (todos, todoText) {
-    const index = todos.findIndex(function (todo, index) {
-
+    const index = todos.findIndex(function (todo) {
         return todo.text.toLowerCase() === todoText.toLowerCase()
     })
-    return todos.splice(index, 1)
+    if (index > -1) {
+        todos.splice(index, 1)
+    }
 }
 
 //Case insensitive search
 //Find a match - delete it.  Delete item from array based on its index.  
-deleteTodo(todos, 'buy food.')
+deleteTodo(todos, 'buy foods.')
 console.log(todos)
