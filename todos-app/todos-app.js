3aa617724ba97124fb2930e5549f5cb2135efad3
Danielle Branton <djabranton@gmail.com>
2022-06-13 15:50:41 -0700

    Created dynamic todos and items.

--- todos-app/todos-app.js
+++ todos-app/todos-app.js
@@ -1,8 +1,48 @@
-const paragraphs = document.querySelectorAll('p')
-// console.log(paragraphs)
+const todos = [{
+        text: 'Let the dogs out.',
+        completed: false
+    },
+    {
+        text: 'Buy food.',
+        completed: true
+    },
+    {
+        text: 'Work out.',
+        completed: true
+    },
 
-paragraphs.forEach(function (p) {
-    if (p.textContent.includes("the")) {
-        p.remove()
+    {
+        text: 'Food shop.',
+        completed: false
+    },
+    {
+        text: 'Cook dinner.',
+        completed: false
     }
+]
+
+//Challenge 2
+//print summary "You have 2 todos left." (p element)
+const totalTodos = document.createElement('p')
+totalTodos.textContent = `You have ${todos.length} todos left.`
+document.querySelector('body').appendChild(totalTodos)
+
+//Add a p for each todo above - use text value for each object
+todos.forEach(function (todo) {
+    let p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
 })
+
+const todo = document.createElement('p')
+
+
+//Challenge 1
+// const paragraphs = document.querySelectorAll('p')
+// // console.log(paragraphs)
+
+// paragraphs.forEach(function (p) {
+//     if (p.textContent.includes("the")) {
+//         p.remove()
+//     }
+// })
